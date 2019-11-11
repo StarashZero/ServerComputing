ab4abab011c5df75da8f270c1e2a88b8b7a3bc0a
StarashZero <StarashZero>
2019-11-11 20:16:17 +0800

    hw5

--- hw5/CloudGo/main.go
+++ hw5/CloudGo/main.go
@@ -7,21 +7,23 @@ import (
 )
 
 const (
+	//set a default port
 	PORT string = "8080"
 )
 
 func main(){
 	port := os.Getenv("PORT")
-
+	//use default port
 	if len(port) == 0{
 		port = PORT
 	}
-
+	//allow user set another port
 	pPort :=  pflag.StringP("port", "p", PORT, "PORT for httpd listening")
 	pflag.Parse()
+	//if user set a port, use it
 	if len(*pPort)!=0{
 		port = *pPort
 	}
-
+	//run server
 	service.NewServer(port)
 }
--- hw5/CloudGo/service/server.go
+++ hw5/CloudGo/service/server.go
@@ -6,15 +6,20 @@ import (
 )
 
 func NewServer(port string) {
+	//get a martini
 	m := martini.Classic()
-	m.Get("/", func(r render.Render) {
-		r.HTML(200, "index", nil)
-	})
+	//set assets path
 	m.Use(martini.Static("assets"))
+	//set render template
 	m.Use(render.Renderer(render.Options{
 		Directory:  "templates",
 		Extensions: []string{".html"},
 		IndentJSON: true,
 	}))
+	//deal with get request
+	m.Get("/", func(r render.Render) {
+		r.HTML(200, "index", nil)
+	})
+	//run martini
 	m.RunOnAddr(":" + port)
 }
